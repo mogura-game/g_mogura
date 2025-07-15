9f3d8a0239265dfe91675dfdfb393c110b01de9a
Fernando Camarena <contacto.fernandoc@gmail.com>
2025-07-15 17:00:59 -0600

    Add: MoguraDigInState
    Add Mogura dig entering State setup.
    Add Mogura dig entering asset from MoguraDigInState.

---
+++ Assets/_Scripts/Mogura/States/MoguraDigInState.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace App.Game.Entities.Mogura {
+    /// <summary>
+    /// Mogura DigEnter class for managing custom State transitions.
+    /// </summary>
+    [CreateAssetMenu(menuName = "States/Mogura/Dig In", fileName = "MoguraDigInState")]
+    public class MoguraDigInState : BaseState {
+    // ? DEBUG======================================================================================================================================
+
+    // ? PARAMETERS=================================================================================================================================
+        // * REFERENCES
+
+        // * ATTRIBUTES
+        [SerializeField] private float timeOnState = 0.0f;
+        
+        // * INTERNAL
+        private PlayerController PC => this.stateMachine.baseController as PlayerController;
+        private PlayerStateMachine SM => this.stateMachine as PlayerStateMachine;
+
+    // ? BASE METHODS===============================================================================================================================
+
+    // ? CUSTOM METHODS=============================================================================================================================
+        
+    // ? EVENT METHODS==============================================================================================================================
+        public override void OnEnter(BaseStateMachine stateMachine) {
+            this.PC.rb.linearVelocity = Vector2.zero;
+            
+            base.OnEnter(stateMachine);
+        }
+
+        public override void OnExecute () {
+            if (this.timeOnState > 5.0f) this.SM.ChangeState(EntityStates.idle);
+            this.timeOnState += Time.fixedDeltaTime;
+
+            base.OnExecute();
+        }
+
+        public override void OnExit() {
+            this.timeOnState = 0.0f;
+
+            base.OnExit();
+        }
+    }
+}
