5cd2a3e2c87baf3a888defc6d1151f39ac8d8253
Fernando Camarena <contacto.fernandoc@gmail.com>
2025-07-15 13:19:38 -0600

    Style: input actions and default controller naming
    Update default Unity input actions naming to match convention.
    Update default Entity controller to match convention using Base since its an abstract class to inherit from instead of a default preset for prefab creation.

---
+++ Assets/_Scripts/Bases/BaseController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace App.Game.Entities {
+    /// <summary>
+    /// Player-specific implementation of the EntityCon oller.
+    /// Handles player-specific state logic and behaviours.
+    /// </summary>
+    [RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D), typeof(Collider2D))]
+    [RequireComponent(typeof(BaseStateMachine))]
+    //[RequireComponent(typeof(EntityAnimator), typeof(Animator))]
+    //[RequireComponent(typeof(EntitySounder), typeof(AudioSource))]
+    public abstract class BaseController : MonoBehaviour {
+    // ? DEBUG======================================================================================================================================
+        [SerializeField] protected bool DEBUG = false;
+
+    // ? PARAMETERS=================================================================================================================================
+        // * REFERENCES
+        [Header("References")]
+        [Tooltip("Reference to the Entity State Machine inherited class controlling this entity.")]
+        [SerializeField] protected BaseStateMachine stateMachine;
+        [Tooltip("Reference to the Animator attached to the Entity.")]
+        [SerializeField] public Animator ani;
+
+        // * ATTRIBUTES
+
+        // * INTERNAL
+        protected EntityStates currentState => this.stateMachine.currentState.id;
+
+    // ? BASE METHODS===============================================================================================================================
+        protected virtual void Awake() {}
+
+        protected virtual void Start() {
+            if (ReferenceEquals(this.stateMachine, null)) if (DEBUG) Debug.Log("[EC] Entity SM initialized");
+            else Debug.LogError("[EC] No StateMachine assigned!");
+        }
+
+        protected virtual void FixedUpdate() {}
+
+        protected virtual void Update() {}
+
+    // ? CUSTOM METHODS=============================================================================================================================
+
+    // ? EVENT METHODS==============================================================================================================================
+
+    }
+}
