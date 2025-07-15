94a654e8ead3f2ee01adfa18f8cde6f78cf38675
Fernando Camarena <contacto.fernandoc@gmail.com>
2025-07-15 16:29:27 -0600

    Add: EntityAnimator setup
    Add BaseAnimator script for managing custom logic and behaviours.
    Add PlayerAnimator script from BaseAnimator to handle custom Mogura player animation logic.

---
+++ Assets/_Scripts/Bases/BaseAnimator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace App.Game.Entities {
+    /// <summary>
+    /// Generic implementation for handling custom animations and Animator managging.
+    /// Inherit from this class to define specific logic and behaviors per Entity.
+    /// </summary>
+    [RequireComponent(typeof(BaseController), typeof(Animator))]
+    public abstract class BaseAnimator : MonoBehaviour {
+    // ? DEBUG======================================================================================================================================
+        [SerializeField] protected bool DEBUG = false;
+
+    // ? PARAMETERS=================================================================================================================================
+        // * REFERENCES
+
+        // * ATTRIBUTES
+        [Tooltip("Reference to the Animator attached to this Entity.")]
+        [SerializeField] public Animator ani;
+
+        // * INTERNAL
+
+    // ? BASE METHODS===============================================================================================================================
+        protected virtual void Awake() {}
+        
+        protected virtual void Start() {}
+        
+        protected virtual void Update() {}
+
+    // ? CUSTOM METHODS=============================================================================================================================
+        public void Play(string stateId) {
+            this.ani.Play(stateId, 0);
+        }
+
+    // ? EVENT METHODS==============================================================================================================================
+
+    }
+}
