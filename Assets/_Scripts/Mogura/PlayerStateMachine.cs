bc2708e383f8329bc557764871a994a11d1b6f01
Fernando Camarena <contacto.fernandoc@gmail.com>
2025-07-15 13:51:39 -0600

    Update: debug and usability
    Update minor misspelling Debug messages on BaseState script.
    Add custom Debug messages on State changing on StateMachines.
    Update BaseStateMachine Entity attribute naming to match convention.
    Update ChangeState method for managing State changing based from EntityStates enum values.
    Update component requirements from multiple classes.

--- Assets/_Scripts/Mogura/PlayerStateMachine.cs
+++ Assets/_Scripts/Mogura/PlayerStateMachine.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 namespace App.Game.Entities.Mogura {
     /// <summary>
     /// Player entity class for implementing a default StateMachine from custom ScriptableObject States.
     /// </summary>
+    [RequireComponent(typeof(PlayerController))]
     public class PlayerStateMachine : BaseStateMachine {
     // ? DEBUG======================================================================================================================================
 
