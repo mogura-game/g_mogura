bc3fe9efbc16f312d791d9c3d5bba96e4bd331c1
Fernando Camarena <contacto.fernandoc@gmail.com>
2025-07-15 16:54:18 -0600

    Add: MoguraIdleState
    Add Mogura idle State inherited from BaseIdle setup.
    Update Mogura idle asset from MoguraIdleState

---
+++ Assets/_Scripts/Mogura/States/MoguraIdleState.cs
@@ -0,0 +1,34 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace App.Game.Entities.Mogura {
+    /// <summary>
+    /// Mogura IdleState class for managing custom State transitions.
+    /// </summary>
+    [CreateAssetMenu(menuName = "States/Mogura/Idle", fileName = "MoguraIdleState")]
+    public class MoguraIdleState : IdleState {
+    // ? DEBUG======================================================================================================================================
+
+    // ? PARAMETERS=================================================================================================================================
+        // * REFERENCES
+
+        // * ATTRIBUTES
+        
+        // * INTERNAL
+        private PlayerController PC => this.stateMachine.baseController as PlayerController;
+        private PlayerStateMachine SM => this.stateMachine as PlayerStateMachine;
+
+    // ? BASE METHODS===============================================================================================================================
+
+    // ? CUSTOM METHODS=============================================================================================================================
+        
+    // ? EVENT METHODS==============================================================================================================================
+        public override void OnEnter(BaseStateMachine stateMachine) {
+            base.OnEnter(stateMachine);
+        }
+
+        public override void OnExecute () {
+            base.OnExecute();
+        }
+    }
+}
